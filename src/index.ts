2b61f45853321910e6fcd8680ef44e75560959e9
ILW8 <daohengliu@gmail.com>
2025-01-12 17:41:58 +0000

    add storing and retrieving match states and primitive auth

--- src/index.ts
+++ src/index.ts
@@ -13,24 +13,59 @@
 
 interface Env {
 	MATCH_STATES: KVNamespace;
+	MATCH_AUTH_MAPPINGS: KVNamespace;
 	// ... other binding types
+}
+
+async function update_state(request: Request<unknown, IncomingRequestCfProperties<unknown>>, env: Env) {
+	if (request.headers.get('Content-Type') !== 'application/json')
+		return new Response('Unsupported Content-Type. ', { status: 415, headers: { 'Allow': 'application/json' } });
+
+	let body: object;
+	let match_id: string;
+	try {
+		body = await request.json();
+		match_id = body['match'];
+	} catch (err) {
+		return Response.json({ error: 'Invalid JSON body.' }, { status: 400 });
+	}
+
+	const authHeader = request.headers.get('Authentication');
+	if (!authHeader)
+		return Response.json({ error: 'Missing Authentication header.' }, { status: 401 });
+
+	const tournament_id = await env.MATCH_AUTH_MAPPINGS.get(authHeader);
+	if (tournament_id == null)
+		return Response.json({ error: 'Invalid Authentication header.' }, { status: 401 });
+
+	await env.MATCH_STATES.put(`${tournament_id}/${match_id}`, JSON.stringify(body));
+
+	return new Response(null, { status: 201 });
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		try {
-			// await env.MATCH_STATES.put("1", JSON.stringify({"hi": "gm"}));
-			await env.MATCH_STATES.put("1", "not a json lol");
-			const value = await env.MATCH_STATES.get("1");
-			if (value === null) {
-				return Response.json({"error": "value not found"}, { status: 404 });
+			if (request.method === 'POST') {
+				return await update_state(request, env);
+			} else if (request.method === 'GET') {
+				const url = new URL(request.url);
+				const key = url.pathname.slice(1); // Remove leading slash
+
+				if (key == null || key === '')
+					return Response.json({ 'error': 'Invalid path.' }, { status: 400 });
+
+				const value = await env.MATCH_STATES.get(key);
+				if (value == null)
+					return Response.json({ 'error': `Match \'${key}\' not found` }, { status: 404 });
+
+				return Response.json(JSON.parse(value));
 			}
-			return Response.json(JSON.parse(value));
 		} catch (err) {
 			// In a production application, you could instead choose to retry your KV
 			// read or fall back to a default code path.
 			console.error(`KV returned error: ${err}`);
-			return Response.json({"error": err.toString()}, { status: 500 });
+			return Response.json({ 'error': err.toString() }, { status: 500 });
 		}
-	},
+	}
 } satisfies ExportedHandler<Env>;

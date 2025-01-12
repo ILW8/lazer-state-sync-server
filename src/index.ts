526ee0082d710eadd86b59884951b9a3377f8b64
ILW8 <daohengliu@gmail.com>
2025-01-12 16:44:54 +0000

    add KV binding and sample KV code

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,26 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface Env {
+	MATCH_STATES: KVNamespace;
+	// ... other binding types
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		try {
+			// await env.MATCH_STATES.put("1", JSON.stringify({"hi": "gm"}));
+			await env.MATCH_STATES.put("1", "not a json lol");
+			const value = await env.MATCH_STATES.get("1");
+			if (value === null) {
+				return Response.json({"error": "value not found"}, { status: 404 });
+			}
+			return Response.json(JSON.parse(value));
+		} catch (err) {
+			// In a production application, you could instead choose to retry your KV
+			// read or fall back to a default code path.
+			console.error(`KV returned error: ${err}`);
+			return Response.json({"error": err.toString()}, { status: 500 });
+		}
 	},
 } satisfies ExportedHandler<Env>;
